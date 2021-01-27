cf243d9a6d8394d2da660c52e43fdf1fae211110
Gus Bikos <gusbikos@gmail.com>
2021-01-27 08:02:57 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -28,7 +28,7 @@ def return_string_array(array)
   count = 0
   new_array = []
   while count < array.length do
-    new_array << array.length.to_s
+    new_array.to_s << array
     count += 1
   end
   new_array
